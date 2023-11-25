48e1540f7ac5852cc976cb3db25d001ae1680738
Maschell <Maschell@gmx.de>
2023-11-25 17:55:10 +0100

    Avoid exploiting the iosu when mocha is already running

--- source/main.cpp
+++ source/main.cpp
@@ -3,12 +3,27 @@
 #include <coreinit/cache.h>
 #include <coreinit/debug.h>
 #include <coreinit/ios.h>
+#include <coreinit/thread.h>
 #include <cstdio>
 #include <cstring>
 #include <mocha/commands.h>
 #include <sysapp/title.h>
 #include <whb/log.h>
 #include <whb/log_udp.h>
+
+static void StartMCPThreadIfMochaAlreadyRunning() {
+    // start /dev/iosuhax and wupserver if mocha is already running
+    int mcpFd = IOS_Open("/dev/mcp", (IOSOpenMode) 0);
+    if (mcpFd >= 0) {
+        int in  = IPC_CUSTOM_START_MCP_THREAD;
+        int out = 0;
+        if (IOS_Ioctl(mcpFd, 100, &in, sizeof(in), &out, sizeof(out)) == IOS_ERROR_OK) {
+            // give /dev/iosuhax a chance to start.
+            OSSleepTicks(OSMillisecondsToTicks(100));
+        }
+        IOS_Close(mcpFd);
+    }
+}
 
 int main(int argc, char **argv) {
     WHBLogUdpInit();
@@ -20,10 +35,11 @@ int main(int argc, char **argv) {
             DCStoreRange((void *) 0xF417FEF0, 0x100);
         }
     }
-
     uint64_t sysmenuIdUll = _SYSGetSystemApplicationTitleId(SYSTEM_APP_ID_WII_U_MENU);
     memcpy((void *) 0xF417FFF0, &sysmenuIdUll, 8);
     DCStoreRange((void *) 0xF417FFF0, 0x8);
+
+    StartMCPThreadIfMochaAlreadyRunning();
 
     ExecuteIOSExploit();
 
