d46b142a2637a624972286d3d5c88da82a8e3108
Jan Hofmeier <jan.hofmeier@gmail.com>
2026-01-31 12:31:10 +0100

    stdcpp -> thread

--- source/crt.c
+++ source/crt.c
@@ -2,7 +2,7 @@ void __init_wut_malloc();
 
 void __init_wut_newlib();
 
-void __init_wut_stdcpp();
+void __init_wut_thread();
 
 void __init_wut_devoptab();
 
@@ -22,7 +22,7 @@ void __attribute__((weak))
 __init_wut_() {
     __init_wut_malloc();
     __init_wut_newlib();
-    __init_wut_stdcpp();
+    __init_wut_thread();
     __init_wut_devoptab();
     if (&__init_wut_socket) __init_wut_socket();
 }
@@ -30,7 +30,6 @@ __init_wut_() {
 void __attribute__((weak))
 __fini_wut_() {
     __fini_wut_devoptab();
-    __fini_wut_stdcpp();
     __fini_wut_newlib();
     __fini_wut_malloc();
 }
